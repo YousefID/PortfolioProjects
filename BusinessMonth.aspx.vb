57e9fa67c34181568b3957daff47a8bbf2f4a2d3
YousefID <32816395+YousefID@users.noreply.github.com>
2022-08-21 18:50:02 +0300

    Add files via upload

---
+++ BusinessMonth.aspx.vb
@@ -0,0 +1,217 @@
+﻿Imports System.IO
+Partial Class Customized_BusinessMonth
+    Inherits System.Web.UI.Page
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ' Check if there is any open business month
+        If Not IsPostBack Then
+
+            If (Utilities.GetLoggedInUser() <> "") Then
+
+                ' Check if the logged user has a Supervisor access
+                If (Authorization.IsUserSupervisor()) Then
+                    ' Supervisor view
+                    MultiView1.ActiveViewIndex = 1
+
+                    ' Fill authorized departments
+                    LoadAuthorizedDepartments(cmbDepartments)
+
+                    ' Set label of Open BM and BM Hours
+                    lblCurrentBMo.Text = BusinessMonth.GetOpenBusinessMonth()
+
+                    lblWorkingHours.Text = BusinessMonth.GetOpenBusinessMonthHours()
+
+
+                ElseIf (Authorization.IsUserSAPHRAdmin()) Then
+                    ' SAP HR Admin view
+                    MultiView1.ActiveViewIndex = 0
+
+                    ' Set label of Open BM
+                    lblOpenBM.Text = BusinessMonth.GetOpenBusinessMonth()
+
+                    ' Set text of BM Hours
+                    txtBMHours.Text = BusinessMonth.GetOpenBusinessMonthHours()
+
+                    ' Are there any open departments in the current open BM?
+                    If (Not BusinessMonth.IsBMDepClosed()) Then
+                        lblOpenBMDep.Visible = True
+
+                        grdOpenDepartments.DataSource = BusinessMonth.GetOpenBMDepartments()
+                        grdOpenDepartments.DataBind()
+
+                        btnGenerateSAPHRReport.Enabled = False
+
+                    End If
+
+                Else
+                    MultiView1.ActiveViewIndex = 2
+                End If
+            Else
+                Response.Redirect("~\Default.aspx")
+            End If
+        End If
+    End Sub
+    Protected Sub LoadAuthorizedDepartments(ByVal DepartmentsControl As DropDownList)
+        ' Enlist the authorized departments
+        Dim dt As DataTable
+        dt = Authorization.GetAuthorizedDepartmentsForSupervisors()
+
+        DepartmentsControl.DataTextField = "DepartmentName"
+        DepartmentsControl.DataValueField = "AccountDepartmentId"
+        DepartmentsControl.DataSource = dt
+        DepartmentsControl.DataBind()
+
+    End Sub
+    Protected Sub btnPostSPRReport_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnPostSPRReport.Click
+
+        ' Posting algorithm
+        ' The name of the text file should be (SPR_BMO_DEPT)
+        Dim sprName As String = "SPR_" & lblCurrentBMo.Text & "_" & cmbDepartments.SelectedItem.Text.Replace(" ", "_")
+        'Dim sprName As String = "SPR"
+
+        ' Loop through records in the grid view         
+        Dim fs As New FileStream(String.Format(Server.MapPath("Uploads\{0}.txt"), sprName), FileMode.Create)
+        Dim sw As New StreamWriter(fs)
+
+        ' Write the columns
+        sw.WriteLine("WorkDate" & vbTab & "Emp. No." & vbTab & "CO.Areea" & vbTab & "CC" & vbTab & "Activity" & vbTab & "Raufnr" & vbTab & "Vornr" & vbTab & "Awart" & vbTab & _
+                     "CatsHours" & vbTab & "ltxa" & vbTab & "Msg" & vbTab & "WBS" & vbTab & "Start Time" & vbTab & "End Time" & vbTab)
+
+        ' Blank Line (intentional, to separate header from data)
+        sw.WriteLine()
+
+        ' NOTE: fields 5, 6, 9 and 10 were replaced with NA due to the unavailability of data!
+        For Each row As GridViewRow In grdSPR.Rows
+
+
+            ' Insert the record (to the database)
+            Spiridon.Insert(grdSPR.DataKeys(row.RowIndex).Value, row.Cells(0).Text, row.Cells(1).Text, row.Cells(2).Text, row.Cells(3).Text, _
+                            row.Cells(4).Text, row.Cells(5).Text, row.Cells(6).Text, row.Cells(7).Text, row.Cells(8).Text, _
+                            row.Cells(9).Text, row.Cells(10).Text, row.Cells(11).Text, row.Cells(12).Text, row.Cells(13).Text, lblCurrentBMo.Text)
+
+            If row.Cells(7).Text <> "0110" Then
+
+                ' Export the record (to the text file)
+                sw.WriteLine(row.Cells(0).Text.Trim() & vbTab & row.Cells(1).Text.Trim() & vbTab & row.Cells(2).Text.Trim() & vbTab & row.Cells(3).Text.Trim() & vbTab & Replace(row.Cells(4).Text.Trim(), "N/A", " ") _
+                             & vbTab & Replace(row.Cells(5).Text.Trim(), "N/A", " ") & vbTab & Replace(row.Cells(6).Text.Trim(), "N/A", " ") & vbTab & Replace(row.Cells(7).Text.Trim(), "N/A", " ") & vbTab & Replace(row.Cells(8).Text.Trim(), "N/A", " ") _
+                             & vbTab & Replace(row.Cells(9).Text.Trim(), "N/A", " ") & vbTab & Replace(row.Cells(10).Text.Trim(), "N/A", " ") & vbTab & Replace(row.Cells(11).Text.Trim(), "N/A", " ") & vbTab & Replace(row.Cells(12).Text.Trim(), "N/A", " ") _
+                             & vbTab & Replace(row.Cells(13).Text.Trim(), "N/A", " "))
+            End If
+        Next
+
+
+
+        ' Close Streams
+        sw.Close()
+        fs.Close()
+
+        For Each rw As GridViewRow In grdSPRAllowance.Rows
+
+            'Insert the record (to database)
+            Spiridon.InsertAllowance(grdSPRAllowance.DataKeys(rw.RowIndex).Value, rw.Cells(0).Text, rw.Cells(1).Text, rw.Cells(2).Text, rw.Cells(3).Text, _
+                           rw.Cells(4).Text, rw.Cells(5).Text, rw.Cells(6).Text, lblCurrentBMo.Text)
+
+
+        Next
+
+        ' Close Business Month
+        BusinessMonth.CloseDepartmentBusinessMonth(cmbDepartments.SelectedValue, lblCurrentBMo.Text)
+
+        ' Link
+        hypSpiridonReport.Visible = True
+        hypSpiridonReport.NavigateUrl = String.Format("~/Customized/Uploads/{0}.txt", sprName)
+
+    End Sub
+    Protected Sub btnShowSpiridonReport_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnShowSpiridonReport.Click
+        ' Hide link
+        hypSpiridonReport.Visible = False
+
+        ' Show Report
+        grdSPR.DataSource = Spiridon.GetReport(cmbDepartments.SelectedValue, _
+                                               calFromDate.SelectedDate.ToShortDateString(), calToDate.SelectedDate.ToShortDateString())
+        grdSPR.DataBind()
+
+
+        grdSPRAllowance.DataSource = Spiridon.GetReportAllowance(cmbDepartments.SelectedValue, _
+                                               calFromDate.SelectedDate.ToShortDateString(), calToDate.SelectedDate.ToShortDateString())
+        grdSPRAllowance.DataBind()
+
+
+        ' Show the POST button if the grid has data
+        If (grdSPR.Rows.Count > 0) Then
+            btnPostSPRReport.Visible = True
+        Else
+            btnPostSPRReport.Visible = False
+        End If
+    End Sub
+    Protected Sub btnGenerateSAPHRReport_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnGenerateSAPHRReport.Click
+
+        Dim dt As DataTable
+
+        dt = SAPHR.GetSAPHRReport(lblOpenBM.Text)
+        If (dt.Rows.Count > 0) Then
+
+            ' Display the SAP HR Report
+            grdSAPHR.DataSource = SAPHR.GetSAPHRReport(lblOpenBM.Text)
+            grdSAPHR.DataBind()
+
+            ' Show the close BM button
+
+            ' Show the Export button
+            imgExportToExcel1.Visible = True
+        End If
+        btnCloseBM.Visible = True
+    End Sub
+    Protected Sub btnUpdateBMHours_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnUpdateBMHours.Click
+        ' Update BM Hours
+        BusinessMonth.UpdateBMHours(txtBMHours.Text)
+
+        ' Update label
+        lblUpdatelabel.Visible = True
+
+    End Sub
+    Protected Sub btnCloseBM_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnCloseBM.Click
+        ' Export the file
+        Dim SAPHRName As String = String.Format("SAPHR_{0}", lblOpenBM.Text)
+        Dim fs As New FileStream(String.Format(Server.MapPath("Uploads\{0}.txt"), SAPHRName), FileMode.Create)
+        Dim sw As New StreamWriter(fs)
+
+        ' Write the columns
+        sw.WriteLine("CA" & Space(2) & "EmpNo" & Space(3) & "EmpName" & Space(18) & "OT" & Space(3) & "Ext" & Space(2) & "Alow" & Space(1) & "Abst" & Space(1) & "Shrt" & Space(1) & _
+                         "OTH" & Space(2))
+
+        ' Write the data
+        ' (NOTE: fields 5,6,9 and 10 were replaced with NA due to the unavailability of data! _
+        ' once the data is available the code needs to be adjusted to align with the other fields)
+        For Each row As GridViewRow In grdSAPHR.Rows
+            sw.WriteLine(Left(row.Cells(1).Text & Space(4), 4) & Left(row.Cells(2).Text & Space(8), 8) & Left(row.Cells(3).Text & Space(25), 25) & _
+                         Right(Space(10) & row.Cells(4).Text, 10) & Right(Space(10) & row.Cells(5).Text, 10) & _
+                         Right(Space(10) & row.Cells(6).Text, 10) & Right(Space(10) & row.Cells(7).Text, 10) & _
+                         Right(Space(10) & row.Cells(8).Text, 10) & Right(Space(10) & row.Cells(9).Text, 10))
+        Next
+
+        ' Close the streams
+        sw.Close()
+        fs.Close()
+
+        ' Link
+        hypSAPHRReport.Visible = True
+        hypSAPHRReport.NavigateUrl = String.Format("~/Customized/Uploads/{0}.txt", SAPHRName)
+
+        ' Close the Business Month
+        BusinessMonth.CloseCurrentBusinessMonth()
+
+        ' Refresh the page
+        'Response.Redirect("~\Customized\BusinessMonth.aspx")
+
+    End Sub
+
+    Protected Sub imgExportToExcel1_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles imgExportToExcel1.Click
+        ' Get the current open Business Month
+        Dim bm As String = BusinessMonth.GetOpenBusinessMonth()
+
+        ' Export
+
+        ExportGridView.exportExcelFile(String.Format("SAPHR_Report_{0}.xls", bm), grdSAPHR)
+
+    End Sub
+End Class
---
+++ BusinessMonth.vb
@@ -0,0 +1,293 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data.SqlClient
+Public Class BusinessMonth
+
+#Region "Data Functions"
+    Public Shared Function GetSPDataTable(ByVal CommandText As String) As DataTable
+        Dim dt As New DataTable
+
+        ' Call stored procedure Ext_Rep_EndOfMonthReport to return a data table result set
+        Dim sqlconn As SqlConnection = _
+            New SqlConnection(ConfigurationManager.ConnectionStrings("LiveConnectionString").ConnectionString)
+
+        ' SQL Command with the  command text
+        Dim sqlcmd As SqlCommand = New SqlCommand(commandText, sqlconn)
+
+        ' SQL Data Adapter
+        Dim sqlda As New SqlDataAdapter(sqlcmd)
+
+        ' Fill the data table with the results from the stored procedure
+        sqlda.Fill(dt)
+
+        Return dt
+    End Function
+    Public Shared Function GetDataTable(ByVal qury As String) As DataTable
+        '..SQL Connection
+        Dim conn As SqlConnection = _
+        New SqlConnection(ConfigurationManager.ConnectionStrings("LiveConnectionString").ConnectionString)
+
+        '..SQL Query
+        Dim sqlText As String = qury
+
+        '..SQL Adapter
+        Dim adapter As New SqlDataAdapter(sqlText, conn)
+
+        '..Data table
+        Dim dtbl As New DataTable()
+
+        '..Fill the adapter
+        adapter.Fill(dtbl)
+
+        '..Return
+        Return dtbl
+    End Function
+    ''' <summary>
+    ''' Executes SQL nonQuery
+    ''' </summary>
+    ''' <param name="query"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Shared Function ExecuteQuery(ByVal query As String) As Boolean
+        '..Return value
+        Dim retVal As Boolean = False
+
+        '..SQL Connection
+        Dim sqlconn As SqlConnection = _
+        New SqlConnection(ConfigurationManager.ConnectionStrings("LiveConnectionString").ConnectionString)
+
+        '..Open Connection
+        sqlconn.Open()
+
+        '..SQL Command
+        Dim sqlcmd As New SqlCommand(query)
+
+        '..Assign SQL Command to a connection
+        sqlcmd.Connection = sqlconn
+
+        '..Execute
+        If (sqlcmd.ExecuteNonQuery() > 0) Then
+            retVal = True
+        Else
+            retVal = False
+        End If
+
+        '..Close Connection
+        sqlconn.Close()
+
+        '..Return
+        Return retVal
+    End Function
+#End Region
+    Public Shared Function CloseCurrentBusinessMonth()
+        Dim BM As String
+        Dim nextBM As String
+        Dim nextBMHours As Integer
+
+        ' Get current BM
+        BM = GetOpenBusinessMonth()
+
+        ' Get next BM
+        nextBM = GetNextBusinessMonth(BM)
+
+        ' Update Ext_BusinessMonth to close the current BM
+        UpdateBusinessMonthStatus("CLOSED", BM)
+
+        ' Calculate the BM Hours for the next BM
+        nextBMHours = GetBusinessMonthHours(nextBM)
+
+        ' Add the next BM to Ext_BusinessMonth 
+        InsertBusinessMonth(nextBM, nextBMHours)
+
+        ' Open the new Business Month for the active departments
+        InsertBMDepartments(nextBM)
+    End Function
+    ''' <summary>
+    ''' Opens a new Business Month for the active departments
+    ''' </summary>
+    ''' <param name="BusinessMonth"></param>
+    ''' <remarks></remarks>
+    Private Shared Sub InsertBMDepartments(ByVal BMonth As String)
+        ExecuteQuery(String.Format("INSERT INTO Ext_BMOpenDep SELECT '{0}' as BMONTH, AccountDepartmentId FROM AccountDepartment WHERE IsDisabled = 0", BMonth))
+    End Sub
+    ''' <summary>
+    ''' Returns the next Business Month after a given Business Month
+    ''' </summary>
+    ''' <param name="BusinessMonth"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Shared Function GetNextBusinessMonth(ByVal BMonth As String) As String
+        Dim BM As String = BMonth
+        Dim nextBM As String
+
+        ' Get next BM
+        If (Right(CType(BM, Integer), 2) = 12) Then
+            nextBM = (CType(BM, Integer) + 89).ToString()
+        Else
+            nextBM = (CType(BM, Integer) + 1).ToString()
+        End If
+
+        Return nextBM
+    End Function
+    ''' <summary>
+    ''' Returns the Open Departments in the current Open BM
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Shared Function GetOpenBMDepartments() As DataTable
+        ' Command Text
+        Dim commandText As String = "exec GetBMOpenDepartments"
+
+        Return GetSPDataTable(commandText)
+    End Function
+    ''' <summary>
+    ''' Returns whether all Departments in the current BM are closed
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Shared Function IsBMDepClosed() As Boolean
+        '..Return value
+        Dim retVal As Boolean
+
+        '..Data table
+        Dim dt As DataTable = GetDataTable(String.Format("SELECT * FROM Ext_BMOpenDep WHERE BMonth = '{0}'", GetOpenBusinessMonth()))
+
+        If (dt.Rows.Count > 0) Then
+            retVal = False
+        Else
+            retVal = True
+        End If
+
+        Return retVal
+    End Function
+    ''' <summary>
+    ''' Update the Business Month hours of the open business month
+    ''' </summary>
+    ''' <param name="BMHours"></param>
+    ''' <remarks></remarks>
+    Public Shared Sub UpdateBMHours(ByVal BMHours As Integer)
+        ExecuteQuery(String.Format("UPDATE Ext_BusinessMonth SET MHOURS = {0} WHERE MSTATUS = 'OPEN'", BMHours))
+    End Sub
+    ''' <summary>
+    ''' Returns the Business Month Hours of the open BM
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Shared Function GetOpenBusinessMonthHours()
+        '..Return value
+        Dim retVal As String = String.Empty
+
+        '..Data table
+        Dim dt As DataTable = GetDataTable("SELECT * FROM Ext_BusinessMonth WHERE MSTATUS = 'OPEN'")
+
+        If (dt.Rows.Count > 0) Then
+            retVal = dt.Rows(0).Item("MHOURS")
+        Else
+            retVal = "na"
+        End If
+
+        Return retVal
+    End Function
+    ''' <summary>
+    ''' Returns the Business Month Hours for a given Business Month
+    ''' </summary>
+    ''' <param name="BusinessMonth"></param>
+    ''' <param name="Factor"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Shared Function GetBusinessMonthHours(ByVal BMonth As String, Optional ByVal Factor As Integer = 9) As Integer
+        ' Business Month days 
+        Dim BMDays As Integer = 0
+
+        ' Business Month (month)
+        Dim bmm As String = Right(BMonth, 2)
+
+        ' Business Month (year)
+        Dim bmy As String = Left(BMonth, 4)
+
+        ' Start date & end date
+        Dim DateStart As Date = String.Format("#{0}/1/{1}#", bmm, bmy)
+        Dim EndDate As Date = String.Format("#{0}/{1}/{2}#", bmm, Date.DaysInMonth(bmy, bmm), bmy)
+
+        ' Thursdays & Fridays in the Business Month
+        Dim thurfri_days As Integer = 0
+
+        ' Looping throug the days of the month to get Thursdays & Fridays
+        For d As Double = DateStart.ToOADate To EndDate.ToOADate
+            If Date.FromOADate(d).DayOfWeek = DayOfWeek.Thursday Or Date.FromOADate(d).DayOfWeek = DayOfWeek.Friday Then
+                thurfri_days += 1
+            End If
+        Next
+
+        ' Business Month Hours = (Days of the Business Month - Thursdays & Fridays) * 9 (hours)
+        BMDays = (Date.DaysInMonth(bmy, bmm) - thurfri_days) * Factor
+
+        ' Return the Business Month days
+        Return BMDays
+    End Function
+    ''' <summary>
+    ''' Closes the Business Month for the given Department
+    ''' </summary>
+    ''' <param name="AccountDepartmentId"></param>
+    ''' <param name="BusinessMonth"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Shared Function CloseDepartmentBusinessMonth(ByVal AccountDepartmentId As String, ByVal BusinessMonth As String)
+        ExecuteQuery(String.Format("DELETE FROM Ext_BMOpenDep WHERE DEP = '{0}' AND BMONTH = '{1}'", AccountDepartmentId, BusinessMonth))
+    End Function
+    ''' <summary>
+    ''' Updates Business Month Status
+    ''' </summary>
+    ''' <param name="MStatus"></param>
+    ''' <param name="BMonth"></param>
+    ''' <remarks></remarks>
+    Public Shared Sub UpdateBusinessMonthStatus(ByVal MStatus As String, ByVal BMonth As String)
+        ExecuteQuery(String.Format("UPDATE Ext_BusinessMonth SET MSTATUS = '{0}' WHERE BMONTH='{1}'", MStatus, BMonth))
+    End Sub
+    ''' <summary>
+    ''' Opens a new Business Month
+    ''' </summary>
+    ''' <param name="BMonth"></param>
+    ''' <param name="MStatus"></param>
+    ''' <param name="MHours"></param>
+    ''' <remarks></remarks>
+    Public Shared Sub InsertBusinessMonth(ByVal BMonth As String, ByVal MHours As String)
+        ExecuteQuery(String.Format("INSERT INTO Ext_BusinessMonth VALUES ('{0}','OPEN',{1})", BMonth, MHours))
+    End Sub
+    ''' <summary>
+    ''' Returns the Business Month Info for the given month.
+    ''' </summary>
+    ''' <param name="BMonth"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Shared Function GetBusinessMonthInfo(ByVal BMonth As String) As DataTable
+
+        '..Data table
+        Dim dt As DataTable = GetDataTable(String.Format("SELECT * FROM Ext_BusinessMonth WHERE BMONTH='{0}'", BMonth))
+
+        '..Return the data table
+        Return dt
+
+    End Function
+    ''' <summary>
+    ''' Returns the currently opened business month if there is any. The opened business month MUST BE closed before opening a new one.
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Shared Function GetOpenBusinessMonth() As String
+        '..Return value
+        Dim retVal As String = String.Empty
+
+        '..Data table
+        Dim dt As DataTable = GetDataTable("SELECT * FROM Ext_BusinessMonth WHERE MSTATUS = 'OPEN'")
+
+        If (dt.Rows.Count > 0) Then
+            retVal = dt.Rows(0).Item("BMONTH")
+        Else
+            retVal = "na"
+        End If
+
+        Return retVal
+
+    End Function
+
+End Class
